f52078f103c1ae47493a35e186af00de2bb11b81
Gearheaddesigns <88631092+Jmerello@users.noreply.github.com>
2024-12-04 08:02:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 // Middleware
 app.use(express.json());
@@ -17,8 +18,6 @@ const db = new sqlite3.Database('./gearheadresources.db', (err) => {
 });
 
 // Basic route
-const path = require('path');
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html')); // Make sure index.html is in the root directory
 });
